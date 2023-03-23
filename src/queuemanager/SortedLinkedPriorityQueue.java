897762a5de0aafe485f2f9e523a00b6221ed923a
Chester <thechez99@gmail.com>
2023-03-23 21:11:50 +0000

    Implements add(), sort() & getHighestPriorityNode()

--- src/queuemanager/SortedLinkedPriorityQueue.java
+++ src/queuemanager/SortedLinkedPriorityQueue.java
@@ -12,6 +12,60 @@ public class SortedLinkedPriorityQueue<T> implements PriorityQueue<T> {
         this.size = 0;
     }
 
+    //Add a new node to the end of the list
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+            PriorityItem<T> priorityItem = new PriorityItem<T>(item, priority);
+
+            Node addNode = new Node(priorityItem);
+            if(isEmpty()){
+                headNode = addNode;
+            } else {
+                Node currentNode = headNode;
+                while(currentNode.nextNode != null){
+                    currentNode = currentNode.nextNode;
+                }
+                currentNode.nextNode = addNode;
+            }
+
+            size++;
+            sort();
+    }
+
+    //Sorts the list by priority
+    public void sort(){
+        Node currentNode = headNode;
+        Node nextNode = headNode.getNextNode();
+        PriorityItem<T> temp;
+
+        while(currentNode != null){
+            while(nextNode != null){
+                if(currentNode.getNodeData().getPriority() > nextNode.getNodeData().getPriority()){
+                    temp = currentNode.getNodeData();
+                    currentNode.setNodeData(nextNode.getNodeData());
+                    nextNode.setNodeData(temp);
+                }
+                nextNode = nextNode.getNextNode();
+            }
+            currentNode = currentNode.getNextNode();
+        }
+    }
+
+    //Gets the highest priority item
+    public T getHighestPriority() throws QueueUnderflowException {
+        if(isEmpty()){
+            throw new QueueUnderflowException();
+        } else{
+            return headNode.getNodeData().getItem();
+        }
+    }
+
+    //Gives back the head node's data
+    @Override
+    public T head() throws QueueUnderflowException {
+        return headNode.getNodeData().getItem();
+    }
+
 
 
     class Node{
@@ -32,6 +86,10 @@ public class SortedLinkedPriorityQueue<T> implements PriorityQueue<T> {
 
         public PriorityItem<T> getNodeData(){
             return nodeData;
+        }
+
+        public void setNodeData(PriorityItem<T> nodeData){
+            this.nodeData = nodeData;
         }
 
         public Node getNextNode(){
