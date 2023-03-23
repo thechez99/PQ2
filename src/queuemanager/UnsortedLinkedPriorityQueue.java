7751ae7a724b81c475efc94b51413910a59b596d
Chester <thechez99@gmail.com>
2023-03-23 21:34:31 +0000

    Implements constructor, add() & head()

--- src/queuemanager/UnsortedLinkedPriorityQueue.java
+++ src/queuemanager/UnsortedLinkedPriorityQueue.java
@@ -2,11 +2,35 @@ package queuemanager;
 
 public class UnsortedLinkedPriorityQueue<T> implements PriorityQueue<T>{
 
+    //Class Variables
+    private Node headNode;
+
+    //Class Constructor(s)
+    public UnsortedLinkedPriorityQueue(){
+        this.headNode = new Node(null, null);
+    }
+
+    //Class Override Function(s)
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+        PriorityItem<T> priorityItem = new PriorityItem<T>(item, priority);
+        Node newNode = new Node(priorityItem, headNode.getNextNode());
+        headNode.setNextNode(newNode.getNextNode());
+    }
+
+    @Override
+    public T head() throws QueueUnderflowException {
+        if(isEmpty())
+            throw new QueueUnderflowException();
+        else
+            return headNode.getNextNode().getNodeData().getItem();
+    }
+
 
 
     class Node{
         private PriorityItem<T> nodeData;
-        private SortedLinkedPriorityQueue.Node nextNode;
+        private SortedLinkedPriorityQueue<T>.Node nextNode;
 
         //Constructor for head node
         public Node(PriorityItem<T> nodeData){
@@ -15,7 +39,7 @@ public class UnsortedLinkedPriorityQueue<T> implements PriorityQueue<T>{
         }
 
         //Constructor for all other nodes
-        public Node(PriorityItem<T> nodeData, SortedLinkedPriorityQueue.Node nextNode){
+        public Node(PriorityItem<T> nodeData, SortedLinkedPriorityQueue<T>.Node nextNode){
             this.nodeData = nodeData;
             this.nextNode = nextNode;
         }
@@ -28,11 +52,11 @@ public class UnsortedLinkedPriorityQueue<T> implements PriorityQueue<T>{
             this.nodeData = nodeData;
         }
 
-        public SortedLinkedPriorityQueue.Node getNextNode(){
+        public SortedLinkedPriorityQueue<T>.Node getNextNode(){
             return nextNode;
         }
 
-        public void setNextNode(SortedLinkedPriorityQueue.Node nextNode){
+        public void setNextNode(SortedLinkedPriorityQueue<T>.Node nextNode){
             this.nextNode = nextNode;
         }
     }
